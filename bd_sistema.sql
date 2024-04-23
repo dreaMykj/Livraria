bc35c4a235f3527e0df580ab179462fe5809a0a3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-22 21:57:33 -0300

    Primeira parte concluida

--- bd_sistema.sql
+++ bd_sistema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 16-Abr-2024 às 03:39
+-- Tempo de geração: 23-Abr-2024 às 02:19
 -- Versão do servidor: 8.0.27
 -- versão do PHP: 8.2.0
 
@@ -37,6 +37,30 @@ CREATE TABLE `tb_cliente` (
   `nr_telefone` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
+--
+-- Extraindo dados da tabela `tb_cliente`
+--
+
+INSERT INTO `tb_cliente` (`cd_cliente`, `nm_cliente`, `nm_endereco`, `nr_endereco`, `nr_cpf`, `nr_cnpj`, `nr_telefone`) VALUES
+(1, 'Gicelly ', 'rua bruna tamagnini de souza ', 880, '1452456875', '42157854257635', '1144524365'),
+(2, 'Gicelly ', 'rua bruna tamagnini de souza ', 252, '45785245789', '45217856952', '4154572');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_editora`
+--
+
+CREATE TABLE `tb_editora` (
+  `cd_editora` int NOT NULL,
+  `nm_editora` varchar(45) NOT NULL,
+  `nm_endereco` varchar(45) NOT NULL,
+  `nr_endereco` int NOT NULL,
+  `nm_bairro` varchar(30) NOT NULL,
+  `nr_telefone` varchar(11) NOT NULL,
+  `nm_gerente` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
 -- --------------------------------------------------------
 
 --
@@ -63,7 +87,10 @@ INSERT INTO `tb_user` (`id_usuario`, `nm_usuario`, `nm_setor`, `login`, `senha`,
 (4, '', '', '', '', ''),
 (5, '', '', '', '', ''),
 (6, '', '', '', '', ''),
-(7, '', '', '', '', '');
+(7, '', '', '', '', ''),
+(8, 'gi', 'ti', 'gi@gmail.com', '12345', '12345'),
+(9, '', '', '', '', ''),
+(10, 'gi', 'ti', 'gi@gmail.com', '123', '123');
 
 --
 -- Índices para tabelas despejadas
@@ -74,6 +101,12 @@ INSERT INTO `tb_user` (`id_usuario`, `nm_usuario`, `nm_setor`, `login`, `senha`,
 --
 ALTER TABLE `tb_cliente`
   ADD PRIMARY KEY (`cd_cliente`);
+
+--
+-- Índices para tabela `tb_editora`
+--
+ALTER TABLE `tb_editora`
+  ADD PRIMARY KEY (`cd_editora`);
 
 --
 -- Índices para tabela `tb_user`
@@ -89,13 +122,19 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT de tabela `tb_cliente`
 --
 ALTER TABLE `tb_cliente`
-  MODIFY `cd_cliente` int NOT NULL AUTO_INCREMENT;
+  MODIFY `cd_cliente` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_editora`
+--
+ALTER TABLE `tb_editora`
+  MODIFY `cd_editora` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_usuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_usuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
